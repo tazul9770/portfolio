933a4053f466808dd48094d512a4022aa9bd41fe
Tazul Islam <linkedin52425@gmail.com>
2025-11-11 19:18:07 +0600

    new committ

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     tech_stack = models.CharField(max_length=200)
+    live_link = models.URLField(blank=True, null=True)
     github_frontend_link = models.URLField(blank=True, null=True)
     github_backend_link = models.URLField(blank=True, null=True)
     image = CloudinaryField('image')
